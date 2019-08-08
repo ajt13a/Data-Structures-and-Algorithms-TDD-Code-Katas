79f79dc690749c2b156b118654933280c0d7f073
ataylor <alexander.taylor2@ngc.com>
2019-08-08 09:21:07 -0400

    Adding InsertionSort

--- DataStructAndAlgos/BubbleSort.cs
+++ DataStructAndAlgos/BubbleSort.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace DataStructAndAlgos
+﻿namespace DataStructAndAlgos
 {
     public class BubbleSort
     {
---
+++ DataStructAndAlgos/InsertionSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DataStructAndAlgos
+{
+    public class InsertionSort
+    {
+        public static int[] Sort(int[] input)
+        {
+            int indexInQuestion;
+            for (int i = 0; i < input.Length; i++)
+            {
+                indexInQuestion = input[i];
+
+                int j = i;
+                while (j > 0 && input[j - 1] > indexInQuestion)
+                {
+                    input[j] = input[j - 1];
+                    j--;
+                }
+                input[j] = indexInQuestion;
+            }
+            return input;
+        }
+    }
+}
